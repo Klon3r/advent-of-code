f3451754c7084db3817b0b356b5932ec861f54af
Klon3r <117739313+Klon3r@users.noreply.github.com>
2025-12-30 02:33:57 +1000

    feat: 06 puzzle - part two

--- 2025/06/06-puzzle.go
+++ 2025/06/06-puzzle.go
@@ -35,6 +35,70 @@ func parseIntoColumns(file []string) [][]string{
 	return columnArray
 }
 
+func parseIntoColumnsPartTwo(file []string) [][]string {
+	maxLineLength := 0
+	for _, line := range file {
+		if len(line) > maxLineLength {
+			maxLineLength = len(line)
+		}
+	}
+	
+	// Initialize column array - each column will be a slice of strings
+	columnArray := make([][]string, maxLineLength)
+	
+	// Get the last row (operators row)
+	lastRowIndex := len(file) - 1
+	
+	// Process each column position from left to right
+	for col := 0; col < maxLineLength; col++ {
+		// Check if there's an operator in the last row at this column
+		var operator string
+		if lastRowIndex >= 0 && col < len(file[lastRowIndex]) {
+			char := string(file[lastRowIndex][col])
+			if char == "+" || char == "*" {
+				operator = char
+				columnArray[col] = append(columnArray[col], operator)
+			}
+		}
+		
+		// Process data rows from top to bottom (excluding the last row)
+		for row := 0; row < lastRowIndex; row++ {
+			// Check if this column position exists in this row
+			if col < len(file[row]) {
+				char := string(file[row][col])
+				// Only add non-space characters
+				if char != " " {
+					columnArray[col] = append(columnArray[col], char)
+				}
+			}
+		}
+	}
+	
+	// Filter out empty columns and return only non-empty ones
+	var result [][]string
+	for _, col := range columnArray {
+		if len(col) > 0 {
+			result = append(result, col)
+		}
+	}
+	
+	return result
+}
+	
+func calc(operator string, count int, num int) int {
+	switch operator {
+	case "+":
+		return count + num
+	case "*":
+		if num == 0 {
+			return count
+		} else {
+		return count * num
+		}
+	}
+	return count
+}
+
 
 func main() {
 	color.Set(color.FgHiMagenta, color.Bold)
@@ -42,7 +106,7 @@ func main() {
 	fmt.Println("05 - Advent Of Code")
 	color.Unset()
 
-	fileName := "06-example.txt"
+	fileName := "06-input.txt"
 	fileArray := utils.ReadFile(fileName)
 	rowLength := len(fileArray)
 	fileCols := parseIntoColumns(fileArray)
@@ -59,13 +123,7 @@ func main() {
 			if i == 0 {
 				count = num
 			} else {
-				switch operator {
-				case "+":
-					count = count + num
-				case "*":
-					count = count * num
-			}
-
+				count = calc(operator, count, num)
 		}
 	}
 
@@ -73,4 +131,39 @@ func main() {
 	}
 	fmt.Println("Answer (Part One):", color.GreenString(strconv.Itoa(total)))
 
+	// Part Two
+	total = 0
+	file := utils.ReadFile(fileName)
+	result := parseIntoColumnsPartTwo(file)
+	var operator string
+	count := 0
+	// var countArray []int
+
+	for _, array := range result {
+		var numArray []string
+		array = append(array, "-")
+		for _, number := range array {
+			if(number == "*" || number == "+") {
+				operator = number
+				total += count
+				count = 0
+			} else {
+				if(number != "-") {
+					numArray = append(numArray, number)
+				}
+				
+			}
+		}
+		join := strings.Join(numArray,"")
+		num, _ := strconv.Atoi(join)
+
+		if (count == 0) {
+			count = num
+		} else {
+			count = calc(operator, count, num)
+		}
+}
+total += count
+
+	fmt.Println("Answer (Part Two):", color.GreenString(strconv.Itoa(total)))
 }
